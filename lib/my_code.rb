90a991847fff6e4cf4829da6047e422ae2ccce54
Ray Reyes <rayreyes5@gmail.com>
2019-09-04 04:20:14 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,8 @@
-# My Code here....
+map_to_negativize(source_array)
+map_to_no_change(source_array)
+map_to_double(source_array)
+map_to_square(source_array)
+
+reduce_to_total(source_array, starting_point)
+reduce_to_all_true(source_array)
+reduce_to_any_true(source_array)
