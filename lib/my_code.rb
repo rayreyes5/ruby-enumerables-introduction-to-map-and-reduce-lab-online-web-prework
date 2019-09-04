cb64cfad1adf4e5707816d31bcb88b9f43be4199
Ray Reyes <rayreyes5@gmail.com>
2019-09-04 04:20:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,3 +3,6 @@ map_to_no_change(source_array)
 map_to_double(source_array)
 map_to_square(source_array)
 
+reduce_to_total(source_array, starting_point)
+reduce_to_all_true(source_array)
+reduce_to_any_true(source_array)
